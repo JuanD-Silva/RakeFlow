0eeed02edd9fecd76c0407113c47b3c7af737537
Juan Silva <silvajuandavid.08@gmail.com>
2026-01-21 16:11:45 -0500

    Fix: Definir variable BASE_URL en axios

--- poker-frontend/src/api/axios.js
+++ poker-frontend/src/api/axios.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+
 
 // URL de tu Backend
 const api = axios.create({
